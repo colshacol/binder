be48311e4bcb5782cd889a30cf37ef674c82d703
Shane Colcleasure <colshacol@gmail.com>
2016-05-31 23:58:37 -0500

    Testing more.

--- build/js/binder.js
+++ build/js/binder.js
@@ -7,10 +7,21 @@ let Binder = {
     },
 
     newHost: (obj) => {
-
+    // host, bindings[]
+      console.log(`the host is ${obj.host}`);
+      console.log(`the bindings are [${obj.bindings}]`)
     }
 
 };
 
 Binder.makeBlue('message');
 Binder.makeRed('para');
+Binder.newHost({
+  host: "app"
+});
+
+
+let myApp = Binder.newHost({
+  host: "para",
+  bindings: ['message', 'words']
+});
